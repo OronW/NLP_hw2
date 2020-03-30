cb3b6f59fb887fccfdee24841b340d1b4b6f8eb2
oronW <oronwerner@gmail.com>
2020-03-30 17:03:55 +0300

    creates distinct user list of current file

--- hw1.py
+++ hw1.py
@@ -8,6 +8,7 @@ directory = r'C:\Users\oron.werner\PycharmProjects\NLP\hw1Input'    # absolute p
 outputDir = r'C:\Users\oron.werner\PycharmProjects\NLP\hw1Output'    # absolute path of folder
 
 
+
 def main(directory, numOfUsers, outputDir):     # directory=sys.argv[1], numOfUsers=sys.argv[2], outputDir=sys.argv[3]
 
     # directory = input()
@@ -20,18 +21,48 @@ def main(directory, numOfUsers, outputDir):     # directory=sys.argv[1], numOfUs
         if currentFile.endswith(".csv"):
             path = directory + '\\' + currentFile
             print(path)
-            file = open(path, 'r', encoding="utf8")
-            lines = file.read()
+            # file = open(path, 'r', encoding="utf8")
             # print(file)     # prints the name of file
+
+            examineFile(path)   # send the current file to work
 
 
             # TODO: change file name to name of user, after list is filled
             f = open(outputDir + "\\" + currentFile[:-4] + '.txt', 'w+')   # create a file with name of "file" .txt.  w+ is write privileges
+            break   # TODO: remove to go over all files. Currently only one file for testings
 
-    print("Total number of files in folder: " + str(os.listdir(directory).__len__()))  # prints number of files
+    # print("Total number of files in folder: " + str(os.listdir(directory).__len__()))  # prints number of files
 
 
 
+def examineFile(file):
+
+    userList = createUserList(file)
+    print('INSIDE')
+    print(userList)
+
+    pass
+
+
+
+
+
+def createUserList(file):
+    with open(file, 'r', encoding="utf8") as csvFile:
+        currentFile = csv.reader(csvFile, delimiter=',')
+
+        userList = []
+        numOfUsers = 0
+        currentUser = ""
+
+        for row in currentFile:
+            # print(currentUser)
+            if row[0] != currentUser:
+                numOfUsers += 1
+                currentUser = row[0]
+                userList.append(row[0])
+
+    return userList
 
 
 main(directory, 3, outputDir)
