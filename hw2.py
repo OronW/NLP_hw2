64228511c4fe2d66ec3f95170e5e713181bcea08
oronW <oronwerner@gmail.com>
2020-04-20 15:26:49 +0300

    cleaned file from hw1 unnecessary functions

--- hw2.py
+++ hw2.py
@@ -12,7 +12,7 @@ directory = r'C:\Users\oron.werner\PycharmProjects\NLP\hw1Output'
 outputDir = r'C:\Users\oron.werner\PycharmProjects\NLP\hw2Output'
 numOfUsers = 10
 
-
+# TODO: creats system args for inputDir and file path before sending
 def main():  # directory=sys.argv[1], numOfUsers=sys.argv[2], outputDir=sys.argv[3]
     print('*********************************')
     numOfUsersToPrint = int(numOfUsers)
@@ -55,28 +55,6 @@ def main():  # directory=sys.argv[1], numOfUsers=sys.argv[2], outputDir=sys.argv
     calcTokenProbability(totalCorpus)
 
 
-def examineFile(filePath, numOfUsersToPrint, outputDir, currentFile):
-    userList = createUserList(filePath)
-
-    userSizeList = []
-    corpus = []
-
-    # get posts by user as dictionary. KEY='user'
-    postsByUser = getPosts(filePath, userList)
-
-    for user in userList:
-        currentUserPosts = postsByUser[user]  # this is an array with all the posts of one user
-        userSentences = analyzePosts(currentUserPosts)
-        postsByUser[user] = userSentences
-        userSizeList.append([len(postsByUser[user]), user])
-
-    usersByNumberOfSentences = sorted(userSizeList, reverse=True)
-
-    corpus = createCorpus(numOfUsersToPrint, usersByNumberOfSentences, postsByUser, outputDir, currentFile)
-    # createUsersFiles(numOfUsersToPrint, usersByNumberOfSentences, postsByUser, outputDir, currentFile)
-
-    return corpus
-
 
 def calcTokenProbability(totalCorpus):
 
@@ -100,7 +78,7 @@ def calcTokenProbability(totalCorpus):
 
     # calcSentenceProbability(tokenProbabilityInFile)
 
-    # TODO: uncomment all these lines for normal use before sending
+
     print('\nUnigrams model based on complete dataset:')
     for i in range(3):
         sentence = createRandomizedSentenceByDistribution(tokenProbabilityInFile)
@@ -183,133 +161,6 @@ def createTokenAppearanceDict(totalCorpus):
     # print(tokenAppearance)
 
     return tokenAppearance, totalWords
-
-
-def createCorpus(numOfUsersToPrint, usersByNumberOfSentences, postsByUser, outputDir, currentFile):
-
-    corpus = []
-
-    # print()
-    # print('Top ' + str(numOfUsersToPrint) + ' users for this file are: ')
-    # print('------------------------------')
-
-
-    for i in range(numOfUsersToPrint):
-        print(usersByNumberOfSentences[i][1])
-        for post in postsByUser[usersByNumberOfSentences[i][1]]:
-            corpus.append(post.lstrip() + ' ' + '<end>')
-
-    return corpus
-    # print(corpus)
-    # f = open(outputDir + "\\" + 'test' + '_' + currentFile[7:-18] + '.txt', 'w+', encoding='utf-8')   #   creates a file with all users for testings
-    # for line in corpus:
-    #     f.write(line + '\n')
-
-
-def createUsersFiles(numOfUsersToPrint, usersByNumberOfSentences, postsByUser, outputDir, currentFile):
-    print()
-    print('Top ' + str(numOfUsersToPrint) + ' users for this file are: ')
-    print('------------------------------')
-
-    f = open(outputDir + "\\" + 'TopUsers' + '_' + currentFile[7:-18] + '.txt', 'w+', encoding='utf-8')  # create a file with name of "file" .txt.  w+ is write privileges
-    for i in range(numOfUsersToPrint):
-        print(usersByNumberOfSentences[i][1])
-        for post in postsByUser[usersByNumberOfSentences[i][1]]:
-            f.write(post.lstrip() + ' ' + '<end>' + '\n')
-
-    print()
-    print('Files written to:')
-    print(outputDir)
-    print()
-
-
-def analyzePosts(userPosts):
-    sentences = []  # this list will contain all the sentences of a user, derived from his/her posts
-
-    userPosts = cleanPosts(userPosts)
-    sentences = makeSentences(userPosts)
-    for sentence in sentences:
-        re.sub('\s\s+', ' ', sentence)
-    sentences = tokenize(sentences)
-
-    return sentences
-
-
-def tokenize(tempSentences):
-    sentences = []
-    for sentence in tempSentences:
-        sentences.append(re.sub('(?<=[.,"\'!?:’])(?=[^\s])|(?=[.,"\'!?:’])(?<=[^\s])', ' ', sentence))
-
-    return sentences
-
-
-def cleanPosts(userPosts):
-    cleanedPosts = []
-
-    for post in userPosts:
-        currentPost = cleanLinks(post)  # if starts with a URL structure - delete the link
-        cleanedPosts.append(currentPost)  # after all is cleaned - append to list
-
-    return cleanedPosts
-
-
-def cleanLinks(post):
-    cleanedPost = ''
-    splitPost = re.split('\s|;', post)
-
-    # list of rules to clean
-    regex = [re.compile('^(http|www)')]
-    regex.append(re.compile('[^\w_\'.?!’]'))
-
-    dontAddPostFlag = False
-
-    for i in splitPost:
-        for j in range(len(regex)):
-            if regex[j].search(i):
-                dontAddPostFlag = True
-
-        if dontAddPostFlag is False:
-            cleanedPost = cleanedPost + ' ' + i
-
-        dontAddPostFlag = False
-
-    return cleanedPost
-
-
-def makeSentences(userPosts):
-    sentences = []
-
-    for post in userPosts:
-        sentences.extend((re.findall('.*?[.?!]+[msMS]?\.?|.+[^\s]', post)))     # this regex split the post by relevant characters, or by the end of the line | working old: '.*?[.?!]+|.+$'
-
-    return sentences
-
-
-def getPosts(filePath, userList):
-    with open(filePath, 'r', encoding="utf8") as csvFile:
-        currentFile = csv.reader(csvFile, delimiter=',')
-
-        postsByUser = {user: [] for user in userList}  # create a dictionary of posts by user. KEY='user'
-
-        for row in currentFile:
-            postsByUser[row[0]].append(row[3])
-
-    return postsByUser
-
-
-def createUserList(filePath):
-    with open(filePath, 'r', encoding="utf8") as csvFile:
-        currentFile = csv.reader(csvFile, delimiter=',')
-
-        userList = []
-        currentUser = ""
-
-        for row in currentFile:
-            if row[0] != currentUser:
-                currentUser = row[0]
-                userList.append(row[0])
-
-    return userList
 
 
 main()
